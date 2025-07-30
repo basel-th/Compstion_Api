38bdb2463b023f8982103872fed6951a85cf9243
Basel Ahmed <baselahmed856@gmail.com>
2025-07-30 11:48:44 +0300

    Lesion 3 Methods

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="home">
     <h1>This Is Home Page</h1>
-    <h2>My Name is {{ name }} - {{ LastName }}</h2>
+    <!-- <h2>My Name is {{ name }} - {{ LastName }}</h2> -->
+    <!-- <p>bleain is {{ active }}</p> -->
+    <!-- <p>object {{ object }}</p> -->
+    <!-- <p>array is {{ array }}</p> -->
     <p>age is {{ age }}</p>
-    <p>bleain is {{ active }}</p>
-    <p>object {{ object }}</p>
-    <p>array is {{ array }}</p>
+    <button @click="increAge">click</button>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-const name = ref("basel");
-const LastName = ref("alqureapy");
+// const name = ref("basel");
+// const LastName = ref("alqureapy");
+// const active = ref(true);
+// const object = ref({
+//   name: "basel",
+//   age: 25,
+//   active: true,
+// });
+// const array = ref(["basel", "alqureapy", 25, true]);
 const age = ref(25);
-const active = ref(true);
-const object = ref({
-  name: "basel",
-  age: 25,
-  active: true,
-});
-const array = ref(["basel", "alqureapy", 25, true]);
+function increAge() {
+  age.value++;
+}
 </script>
