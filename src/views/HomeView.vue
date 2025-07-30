f4250ab7b40442d76e7ed7efc03fe43d189a4678
Basel Ahmed <baselahmed856@gmail.com>
2025-07-30 11:30:09 +0300

    Lessenon 1 Ref and Reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>This Is Home Page</h1>
+    <h2>My Name is {{ name }} - {{ LastName }}</h2>
+    <p>age is {{ age }}</p>
+    <p>bleain is {{ active }}</p>
+    <p>object {{ object }}</p>
+    <p>array is {{ array }}</p>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-};
+<script setup>
+import { ref } from "vue";
+const name = ref("basel");
+const LastName = ref("alqureapy");
+const age = ref(25);
+const active = ref(true);
+const object = ref({
+  name: "basel",
+  age: 25,
+  active: true,
+});
+const array = ref(["basel", "alqureapy", 25, true]);
 </script>
