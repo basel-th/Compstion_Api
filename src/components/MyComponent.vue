18c9a47a01fc2392ae18616681b37ef8f0328b45
Basel <Baselahmed856@gamil.comu@example.com>
2025-07-31 09:04:25 +0300

    lessen 6 Props

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <dev class="comp">
+    <h2>Thus is commpenet</h2>
+    <p>name is {{ props.userName }} , age is {{ props.userAge }}</p>
+  </dev>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps({
+  userName: {
+    type: String,
+    required: true,
+  },
+  userAge: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,13 +7,15 @@
     <!-- <p>array is {{ array }}</p> -->
     <!-- <p>age is {{ age }}</p> -->
     <!-- <button @click="increAge">click</button> -->
-    <p>{{ UserWelcome }}</p>
-    <p>{{ ageAfter5Years }}</p>
+    <!-- <p>{{ UserWelcome }}</p>
+    <p>{{ ageAfter5Years }}</p> -->
+    <the-comp :userName="name" :userAge="age"></the-comp>
   </div>
 </template>
 
 <script setup>
-import { computed, ref } from "vue";
+import { ref } from "vue";
+import TheComp from "@/components/MyComponent.vue";
 // const name = ref("basel");
 // const LastName = ref("alqureapy");
 // const active = ref(true);
@@ -29,11 +31,11 @@ import { computed, ref } from "vue";
 const name = ref("Basel Ahmed");
 const age = ref(25);
 
-const UserWelcome = computed(
-  () => `Heollo ${name.value}, youre age is ${age.value}`
-);
+// const UserWelcome = computed(
+//   () => `Heollo ${name.value}, youre age is ${age.value}`
+// );
 
-const ageAfter5Years = computed(() => {
-  return `your age after 5 years will be ${age.value + 5}`;
-});
+// const ageAfter5Years = computed(() => {
+//   return `your age after 5 years will be ${age.value + 5}`;
+// });
 </script>
